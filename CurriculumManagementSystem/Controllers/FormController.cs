22a848bdffacefdd9d3f3f6ebb4614b7f894095f
Pham Minh Tung <phamminhtung111097@gmail.com>
2018-11-11 17:59:41 +0700

    Update Form and Error404

---
+++ CurriculumManagementSystem/Controllers/FormController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CurriculumManagementSystem.Controllers
+{
+    public class FormController : Controller
+    {
+        // GET: Form
+        public ActionResult FormCommon()
+        {
+            return View();
+        }
+        public ActionResult FormValidation()
+        {
+            return View();
+        }
+        public ActionResult FormWizard()
+        {
+            return View();
+        }
+    }
+}
